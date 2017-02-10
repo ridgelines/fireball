6e21271d0b6199c5104ae1c095f6b6a32ab54661
zpatrick <zpatrick319@hotmail.com>
2017-02-10 00:20:51 +0000

    add preflight headers

--- context.go
+++ context.go
@@ -10,7 +10,7 @@ var (
 	TextHeaders = map[string]string{"Content-Type": "text/plain"}
 	CORSHeaders = map[string]string{
 		"Access-Control-Allow-Origin":  "*",
-		"Access-Control-Allow-Headers": "*",
+		"Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
 		"Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, COPY, HEAD, OPTIONS, LINK, UNLINK, CONNECT, TRACE, PURGE",
 	}
 )
