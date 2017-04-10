9fb0836a1d0815a3c4e83fd31c1e75f80a213abd
zpatrick <zpatrick319@hotmail.com>
2017-04-10 20:56:13 +0000

    migrate movie model

--- examples/api/controllers/movie.go
+++ examples/api/controllers/movie.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/zpatrick/fireball"
-	"github.com/zpatrick/fireball/examples/swagger/models"
+	"github.com/zpatrick/fireball/examples/api/models"
 )
 
 type MovieController struct {
---
+++ examples/api/models/movie.go
@@ -0,0 +1,6 @@
+package models
+
+type Movie struct {
+	Title string `json:"title"`
+	Year  int    `json:"year"`
+}
