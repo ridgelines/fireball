8dcbce400f0c125bd43e7d5cc136b6b730a5aba2
Zack Patrick <zpatrick319@Hotmail.com>
2018-08-30 12:07:07 -0700

    add ReadJSONResponse test helper:

---
+++ test_helpers.go
@@ -0,0 +1,20 @@
+package fireball
+
+import (
+	"encoding/json"
+	"net/http/httptest"
+	"testing"
+
+)
+
+func ReadJSONResponse(t *testing.T, resp Response, v interface{}) *httptest.ResponseRecorder {
+	recorder := httptest.NewRecorder()
+	resp.Write(recorder, nil)
+	if v != nil {
+		if err := json.Unmarshal(recorder.Body.Bytes(), v); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	return recorder
+}
